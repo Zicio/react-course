a82c5d953d4188b2f3e1ff6eac81bf0f8c453eca
Zicio <zicio@yandex.ru>
2023-12-07 11:46:02 +0400

    2.11. AppRouter. Config for router

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,9 +1,11 @@
 import { lazy } from "react";
 
-export const AboutPageAsync = lazy(
+const AboutPageAsync = lazy(
   () =>
     new Promise((resolve) => {
       // @ts-ignore
       setTimeout(() => resolve(import("./AboutPage")), 1500);
     })
 );
+
+export default AboutPageAsync;
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,9 +1,11 @@
 import { lazy } from "react";
 
-export const MainPageAsync = lazy(
+const MainPageAsync = lazy(
   () =>
     new Promise((resolve) => {
       // @ts-ignore
       setTimeout(() => resolve(import("./MainPage")), 1500);
     })
 );
+
+export default MainPageAsync;
