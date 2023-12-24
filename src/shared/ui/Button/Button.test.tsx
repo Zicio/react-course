8dec12ffcf9d66f5ec3f0c08e1f0370071f5d86e
Alexander Burachkov <zicio@yandex.ru>
2023-12-24 23:23:05 +0400

    3.24.1. React Testing Library. Switching to WS

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,9 +1,14 @@
-import { Button } from './Button';
+import { Button, ThemeButton } from './Button';
 import { render, screen } from '@testing-library/react';
 
 describe('Button', () => {
-  test('with only first param', () => {
+  test('render button', () => {
     render(<Button>Test</Button>);
     expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+
+  test('check theme', () => {
+    render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
+    expect(screen.getByText('Test')).toHaveClass('clear');
   });
 });
