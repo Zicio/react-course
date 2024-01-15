46366739a2b7099cc8d994bbd7f9356924aca963
Zicio <zicio@yandex.ru>
2024-01-15 16:44:26 +0400

    3.28.1. Sidebar with Links

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,10 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/main.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -33,9 +37,31 @@ export const Sidebar = ({ className }: SidebarProps) => {
             >
                 {collapsed ? '>' : '<'}
             </Button>
+            <nav className={cls.items}>
+                <div className={cls.item}>
+                    <MainIcon className={cls.icon} />
+                    <AppLink
+                        theme={AppLinkTheme.SECONDARY}
+                        to={RoutePath.main}
+                        className={cls.link}
+                    >
+                        {t('Главная')}
+                    </AppLink>
+                </div>
+                <div className={cls.item}>
+                    <AboutIcon className={cls.icon} />
+                    <AppLink
+                        theme={AppLinkTheme.RED}
+                        to={RoutePath.about}
+                        className={cls.link}
+                    >
+                        {t('О сайте')}
+                    </AppLink>
+                </div>
+            </nav>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher className={cls.lang} short={collapsed} />
             </div>
         </div>
     );
