7afd9485aaa7a60e35ce9f5b52d2964a5d817cf3
Zicio <zicio@yandex.ru>
2024-01-25 16:50:07 +0400

    4.32.0. Custom input

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,15 +1,15 @@
 import { counterActions, counterReducer } from './counterSlice';
-import { counterSchema } from '../types/counterSchema';
+import { CounterSchema } from '../types/CounterSchema';
 
 describe('counterSlice.test', () => {
   test('decrement', () => {
-    const state: counterSchema = {
+    const state: CounterSchema = {
       value: 10,
     };
     expect(counterReducer(state, counterActions.decrement)).toEqual({ value: 9 });
   });
   test('increment', () => {
-    const state: counterSchema = {
+    const state: CounterSchema = {
       value: 10,
     };
     expect(counterReducer(state, counterActions.increment)).toEqual({ value: 11 });
