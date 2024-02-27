cefa581df9326cc5d95a7a452e00f8be88f1e7ac
Zicio <zicio@yandex.ru>
2024-02-27 14:05:15 +0400

    5.39.0. ProfileCard. Data fetching. TS strict mode.

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { IProfileSchema } from '../types/profile';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { IProfile, IProfileSchema } from '../types/profile';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
 
 const initialState: IProfileSchema = {
   readonly: true,
@@ -11,8 +12,20 @@ const initialState: IProfileSchema = {
 export const profileSlice = createSlice({
   name: 'profile',
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProfileData.pending, (state) => {
+      state.error = undefined;
+      state.isLoading = true;
+    })
+      .addCase(fetchProfileData.fulfilled, (state, action: PayloadAction<IProfile>) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchProfileData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,14 +1,14 @@
 import { Country, Currency } from 'shared/const/common';
 
 export interface IProfile {
-  'first': string,
-  'lastname': string,
-  'age': 22,
-  'currency': Currency,
-  'country': Country,
-  'city': string,
-  'username': string,
-  'avatar': string
+  first: string,
+  lastname: string,
+  age: 22,
+  currency: Currency,
+  country: Country,
+  city: string,
+  username: string,
+  avatar: string
 }
 
 export interface IProfileSchema {
